91d2300076a9c653551b5359b3dbcfa2d53fd697
louis.magowan <louis.magowan@qonto.com>
2023-08-18 18:32:32 +0200

    chore: Trying to get scipy import working with app

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import plotly.express as px
-import scipy
+from scipy.stats import weibull_min
 from sklearn.preprocessing import MinMaxScaler
 # ------------------- VARIABLE TRANSFORMATION FUNCTIONS ------------------------
 
@@ -46,7 +46,7 @@ def weibull_pdf_adstock_decay(impact, shape, scale, periods):
     transformed_scale = np.percentile(range(1, periods), scale * 100) 
     
     # Create a Weibull distribution object with the transformed scale parameter
-    weibull_dist = scipy.stats.weibull_min(shape, scale=transformed_scale)
+    weibull_dist = weibull_min(shape, scale=transformed_scale)
 
     # Calculate adstock decay values for each period using the Weibull PDF
     for t in range(1, periods+1):
