1d8408697f58b98e6d4ba1bc6ec9b782154251e9
louis.magowan <louis.magowan@qonto.com>
2023-08-23 11:48:49 +0200

    feat: Added Weibull CDF, reworked the PDF into one function that's much more efficient. Also, added keys to sliders so the display text for them can be identical

--- app.py
+++ app.py
@@ -26,38 +26,51 @@ def geometric_adstock(impact, decay_factor, periods):
     
     return adstock_values
 
-def weibull_pdf_adstock_decay(impact, shape, scale, periods):
+def weibull_adstock_decay(impact, shape, scale, periods, adstock_type='cdf', normalised=True):
     """
-    Calculate Weibull PDF adstock decay for media mix modeling.
+    Calculate the Weibull PDF or CDF adstock decay for media mix modeling.
 
     Parameters:
         impact (float): Initial advertising impact.
         shape (float): Shape parameter of the Weibull distribution.
         scale (float): Scale parameter of the Weibull distribution.
         periods (int): Number of periods.
+        adstock_type (str): Type of adstock ('cdf' or 'pdf').
+        normalise (bool): If True, normalises decay values between 0 and 1,
+                        otherwise leaves unnormalised.
 
     Returns:
-        list: List of adstock-transformed values for each period.
+        list: List of adstock-decayed values for each period.
     """
-    # List to store decay values
-    adstock_decay_values = []
+    # Create an array of time periods
+    x_bin = np.arange(1, periods + 1)
 
     # Transform the scale parameter according to percentile of time period
-    transformed_scale = np.percentile(range(1, periods), scale * 100) 
-    
-    # Create a Weibull distribution object with the transformed scale parameter
-    weibull_dist = weibull_min(shape, scale=transformed_scale)
+    transformed_scale = round(np.quantile(x_bin, scale))
 
-    # Calculate adstock decay values for each period using the Weibull PDF
-    for t in range(1, periods+1):
-        adstock_decay = impact * weibull_dist.pdf(t)
-        adstock_decay_values.append(adstock_decay)
-
-    # Normalize adstock decay values for easier plotting
-    reshaped_decay = np.array(adstock_decay_values).reshape(-1, 1)
-    normalised_adstock_decay = MinMaxScaler().fit_transform(reshaped_decay).flatten()
+    # Handle the case when shape or scale is 0
+    if shape == 0 or scale == 0:
+        theta_vec_cum = np.zeros(periods)
+    else:
+        if adstock_type.lower() == 'cdf':
+            # Calculate the Weibull adstock decay using CDF
+            theta_vec = np.concatenate(([1], 1 - weibull_min.cdf(x_bin[:-1], shape, scale=transformed_scale)))
+            theta_vec_cum = np.cumprod(theta_vec)
+        elif adstock_type.lower() == 'pdf':
+            # Calculate the Weibull adstock decay using PDF
+            theta_vec_cum = weibull_min.pdf(x_bin, shape, scale=transformed_scale)
+            theta_vec_cum /= np.sum(theta_vec_cum)
     
-    return normalised_adstock_decay
+    # Return adstock decay values, normalized or not
+    if normalised:
+        # Normalize the values between 0 and 1 using Min-Max scaling
+        norm_theta_vec_cum = MinMaxScaler().fit_transform(theta_vec_cum.reshape(-1, 1)).flatten()
+        # Scale by initial impact variable
+        return norm_theta_vec_cum * impact
+    else:
+        # Scale by initial impact variable
+        return theta_vec_cum * impact
+
 
 # -------------------------- TOP OF PAGE INFORMATION -------------------------
 
@@ -84,7 +97,7 @@ with tab1:
 
     # User inputs
     st.subheader('User Inputs')
-    num_periods = st.slider('Geometric - Number of weeks after impressions first received :alarm_clock:', 1, 100, 20)
+    num_periods = st.slider('Number of weeks after impressions first received :alarm_clock:', 1, 100, 20, key = "Geometric")
     # Let user choose 3 decay rates to compare simultaneously
     decay_rate_1 = st.slider(':blue[Beta 1] :large_blue_square:', 0.0, 1.0, 0.3)
     decay_rate_2 = st.slider(':red[Beta 2] :large_red_square:', 0.0, 1.0, 0.6)
@@ -111,7 +124,7 @@ with tab1:
     # Plot adstock values
     # Annotate the plot if user wants it
     st.markdown('**Would you like to show the adstock values directly on the plot?**')
-    annotate = st.checkbox('Yes please! :pray:')
+    annotate = st.checkbox('Yes please! :pray:', key = "Geometric Annotate")
     if annotate:
         fig = px.line(all_adstocks, x = 'Week',
                 y = 'Adstock', text = 'Adstock Labels',
@@ -142,6 +155,68 @@ with tab2:
     st.header('Weibull CDF Adstock Transformation')
     # User inputs
     st.subheader('User Inputs')
+    num_periods_2 = st.slider('Number of weeks after impressions first received :alarm_clock:',
+                               1, 100, 20, key = "Weibull CDF Periods")
+    # Let user choose shape and scale parameters to compare two Weibull PDF decay curves simultaneously
+    # Params for Line A
+    shape_parameter_A = st.slider(':triangular_ruler: :blue[Shape of Line A] :large_blue_square:', 
+                                  0.0, 10.0, 2.0, key = "Weibull CDF Shape A")
+    scale_parameter_A = st.slider(':blue[Scale of Line A] :large_blue_square:',
+                                   0.0, 1.0, 0.5, key = "Weibull CDF Scale A")
+    # Params for Line B
+    shape_parameter_B = st.slider(':triangular_ruler: :red[Shape of Line B] :large_red_square:', 
+                                  0.0, 10.0, 0.5, key = "Weibull CDF Shape B")
+    scale_parameter_B = st.slider(':red[Scale of Line B] :large_red_square:', 
+                                  0.0, 1.0, 0.01, key = "Weibull CDF Scale B")
+
+    # Calculate weibull pdf adstock values, decayed over time for both sets of params
+    adstock_series_A = weibull_adstock_decay(initial_impact, shape_parameter_A,
+                                                  scale_parameter_A, num_periods_2,
+                                                  adstock_type='cdf', normalised=True)
+    
+    adstock_series_B = weibull_adstock_decay(initial_impact, shape_parameter_B,
+                                                  scale_parameter_B, num_periods_2,
+                                                  adstock_type='cdf', normalised=True)
+
+    # Create dfs of both sets of adstock values, to plot with
+    adstock_df_A = pd.DataFrame({"Week": range(1, (num_periods_2 + 1)),
+                                "Adstock": adstock_series_A,
+                                "Line": "Line A"})
+    adstock_df_B = pd.DataFrame({"Week": range(1, (num_periods_2 + 1)),
+                                "Adstock": adstock_series_B,
+                                "Line": "Line B"})
+    # Create plotting df
+    weibull_pdf_df = pd.concat([adstock_df_A, adstock_df_B])
+    # Multiply by 100 to get back to scale of initial impact (100 FB impressions)
+    weibull_pdf_df.Adstock = weibull_pdf_df.Adstock
+    # Format adstock labels for neater plotting
+    weibull_pdf_df["Adstock Labels"] = weibull_pdf_df.Adstock.map('{:,.0f}'.format)
+
+    # Plot adstock values
+    # Annotate the plot if user wants it
+    st.markdown('**Would you like to show the adstock values directly on the plot?**')
+    annotate = st.checkbox('Yes please! :pray:', key = "Weibull CDF Annotate")
+    if annotate:
+        fig = px.line(weibull_pdf_df, x = 'Week',
+                y = 'Adstock', text = 'Adstock Labels',
+                markers=True, color = "Line",
+                # Replaces default color mapping by value
+                color_discrete_map={"Line A": "#636EFA",
+                                        "Line B": "#EF553B"})
+        fig.update_traces(textposition="bottom left")
+    else:
+        fig = px.line(weibull_pdf_df, x = 'Week',
+                y = 'Adstock',
+                markers=True, color = "Line",
+                # Replaces default color mapping by value
+                color_discrete_map={"Line A": "#636EFA",
+                                        "Line B": "#EF553B"})
+    # Format plot
+    fig.layout.height = 600
+    fig.layout.width = 1000
+    fig.update_layout(title_text="Weibull CDF Adstock Decayed Over Weeks", 
+                    title_font = dict(size = 30))
+    st.plotly_chart(fig, theme="streamlit", use_container_width=False)
 
 # -------------------------- WEIBULL PDF ADSTOCK DISPLAY -------------------------
 with tab3:
@@ -149,20 +224,28 @@ with tab3:
 
     # User inputs
     st.subheader('User Inputs')
-    num_periods_3 = st.slider('Weibull PDF - Number of weeks after impressions first received :alarm_clock:', 1, 100, 20)
+    num_periods_3 = st.slider('Number of weeks after impressions first received :alarm_clock:',
+                               1, 100, 20, key = "Weibull PDF Periods")
     # Let user choose shape and scale parameters to compare two Weibull PDF decay curves simultaneously
     # Params for Line A
-    shape_parameter_A = st.slider(':triangular_ruler: :blue[Shape of Line A] :large_blue_square:', 0.0, 10.0, 2.0)
-    scale_parameter_A = st.slider(':blue[Scale of Line A] :large_blue_square:', 0.0, 1.0, 0.5)
+    shape_parameter_A = st.slider(':triangular_ruler: :blue[Shape of Line A] :large_blue_square:',
+                                   0.0, 10.0, 2.0, key = "Weibull PDF Shape A")
+    scale_parameter_A = st.slider(':blue[Scale of Line A] :large_blue_square:', 
+                                  0.0, 1.0, 0.5, key = "Weibull PDF Scale A")
     # Params for Line B
-    shape_parameter_B = st.slider(':triangular_ruler: :red[Shape of Line B] :large_red_square:', 0.0, 10.0, 0.5)
-    scale_parameter_B = st.slider(':red[Scale of Line B] :large_red_square:', 0.0, 1.0, 0.01)
+    shape_parameter_B = st.slider(':triangular_ruler: :red[Shape of Line B] :large_red_square:',
+                                   0.0, 10.0, 0.5, key = "Weibull PDF Shape B")
+    scale_parameter_B = st.slider(':red[Scale of Line B] :large_red_square:', 
+                                  0.0, 1.0, 0.01, key = "Weibull PDF Scale B")
 
     # Calculate weibull pdf adstock values, decayed over time for both sets of params
-    adstock_series_A = weibull_pdf_adstock_decay(initial_impact, shape_parameter_A,
-                                                  scale_parameter_A, num_periods_3)
-    adstock_series_B = weibull_pdf_adstock_decay(initial_impact, shape_parameter_B,
-                                                  scale_parameter_B, num_periods_3)
+    adstock_series_A = weibull_adstock_decay(initial_impact, shape_parameter_A,
+                                                  scale_parameter_A, num_periods_3,
+                                                  adstock_type='pdf', normalised=True)
+    
+    adstock_series_B = weibull_adstock_decay(initial_impact, shape_parameter_B,
+                                                  scale_parameter_B, num_periods_3,
+                                                  adstock_type='pdf', normalised=True)
 
     # Create dfs of both sets of adstock values, to plot with
     adstock_df_A = pd.DataFrame({"Week": range(1, (num_periods_3 + 1)),
@@ -174,14 +257,14 @@ with tab3:
     # Create plotting df
     weibull_pdf_df = pd.concat([adstock_df_A, adstock_df_B])
     # Multiply by 100 to get back to scale of initial impact (100 FB impressions)
-    weibull_pdf_df.Adstock = weibull_pdf_df.Adstock * 100
+    weibull_pdf_df.Adstock = weibull_pdf_df.Adstock
     # Format adstock labels for neater plotting
     weibull_pdf_df["Adstock Labels"] = weibull_pdf_df.Adstock.map('{:,.0f}'.format)
 
     # Plot adstock values
     # Annotate the plot if user wants it
     st.markdown('**Would you like to show the adstock values directly on the plot?**')
-    annotate = st.checkbox('Yes please! :grinning:')
+    annotate = st.checkbox('Yes please! :pray:', key = "Weibull PDF Annotate")
     if annotate:
         fig = px.line(weibull_pdf_df, x = 'Week',
                 y = 'Adstock', text = 'Adstock Labels',
