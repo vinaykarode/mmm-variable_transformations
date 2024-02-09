98d4d11b2797b5698a61bdc403ad794a1086d404
louis.magowan <louis.magowan@qonto.com>
2024-02-09 15:16:48 +0100

    feat: Adding geometric delayed adstock function

--- Adstock.py
+++ Adstock.py
@@ -6,7 +6,7 @@ from scipy.stats import weibull_min
 from sklearn.preprocessing import MinMaxScaler
 # ------------------- VARIABLE TRANSFORMATION FUNCTIONS ------------------------
 
-def geometric_adstock(impact, decay_factor, periods):
+def geometric_adstock_decay(impact, decay_factor, periods):
     """
     Calculate the geometric adstock effect.
 
@@ -23,6 +23,32 @@ def geometric_adstock(impact, decay_factor, periods):
     for _ in range(1, periods):
         impact *= decay_factor
         adstock_values.append(impact)
+    
+    return adstock_values
+
+def delayed_geometric_decay(impact, decay_factor, theta, L):
+    """
+    Calculate the geometric adstock effect with a delayed peak and a specified maximum lag length.
+    
+    Parameters:
+        impact (float): Peak advertising impact.
+        decay_factor (float): Decay factor between 0 and 1, applied throughout.
+        theta (int): Period at which peak impact occurs.
+        L (int): Maximum lag length for adstock effect.
+        
+    Returns:
+        np.array: Array of adstock values for each lag up to L.
+    """
+    adstock_values = np.zeros(L)
+    
+    # Calculate adstock values
+    for lag in range(L):
+        if lag < theta:
+            # Before peak, apply decay to grow towards peak
+            adstock_values[lag] = impact * (decay_factor ** abs(lag - theta))
+        else:
+            # After peak, apply decay normally
+            adstock_values[lag] = impact * (decay_factor ** abs(lag - theta))
     
     return adstock_values
 
@@ -85,9 +111,15 @@ st.title('Adstock Transformations')
 st.markdown("This page demonstrates the effect of various adstock \
             transformations on a variable.  \nFor these examples, let's imagine \
             that we have _some variable that represents a quantity of a particlar_ \
-            _advertising channel_.  \nFor example, this could be the number of impressions\
-            we get from Facebook.  \nSo, at the start of our example (_Week 1_), \
-            we would have **100 impressions from Facebook**. \
+            _advertising channel_.  \n\nFor example, this could be the number of impressions\
+            we get from Facebook.  For an online channel such as this, we might expect the impact of these ads to be immediate: \
+            \n   ___We see an ad on Facebook - we either click on it, or we don't.___  \n\
+            \n :blue[So, at the start of our example (_Week 1_), \
+            we could have the impact of **100 impressions from Facebook**.] \
+            \n\n Alternatively, for a channel like TV, we may not expect the impact of those ads  \n \
+            to come through immediately - there may be some delay. \
+            \n\n :green[So, at the start of our example (_Week 1_), we may have the impact of **0 Gross Rating Points (TV viewership metric)**, \
+            but 7 weeks later those TV ads might reach their full impact of **100 Gross Rating Points**.]\
             \n\n**_:violet[We will use this starting value of 100 for all of our adstock examples]_**. \
             ")
 
@@ -99,7 +131,7 @@ st.markdown("**Reminder:** \n \
 initial_impact = 100
 
 # Separate the adstock transformations into 3 tabs
-tab1, tab2, tab3 = st.tabs(["Geometric", "Weibull CDF", "Weibull PDF"])
+tab1, tab2, tab3, tab4 = st.tabs(["Geometric", "Delayed Geometric", "Weibull CDF", "Weibull PDF"])
 
 # -------------------------- GEOMETRIC ADSTOCK DISPLAY -------------------------
 with tab1:
@@ -113,13 +145,19 @@ with tab1:
     # User inputs
     st.subheader('User Inputs')
     num_periods = st.slider('Number of weeks after impressions first received :alarm_clock:', 1, 100, 20, key = "Geometric")
-    # Let user choose 3 decay rates to compare simultaneously
+    # Let user choose decay rates to plot with
     decay_rate_1 = st.slider(':blue[Beta 1] :large_blue_square:', 0.0, 1.0, 0.3)
-    decay_rate_2 = st.slider(':red[Beta 2] :large_red_square:', 0.0, 1.0, 0.6)
-    decay_rate_3 = st.slider(':green[Beta 3] :large_green_square:', 0.0, 1.0, 0.9)
-
+    # Add up to 2 more lines if the user wants it
+    st.markdown('**Would you like to show multiple (3) decay lines on the plot**')
+    multi_plot = st.checkbox('Okay! :grin:')
     # Create a list of decay rates
-    decay_rates = [decay_rate_1, decay_rate_2, decay_rate_3]
+    if multi_plot:
+        # Let user choose additional decay rates to plot with
+        decay_rate_2 = st.slider(':red[Beta 2] :large_red_square:', 0.0, 1.0, 0.6)
+        decay_rate_3 = st.slider(':green[Beta 3] :large_green_square:', 0.0, 1.0, 0.9)
+        decay_rates = [decay_rate_1, decay_rate_2, decay_rate_3]
+    else:
+        decay_rates = [decay_rate_1]
 
     # Create df to store each adstock in
     all_adstocks = pd.DataFrame()
@@ -128,9 +166,9 @@ with tab1:
         # Get geometric adstock values, decayed over time
         adstock_df = pd.DataFrame({"Week": range(1, (num_periods + 1)),
                                 ## Calculate adstock values
-                                    "Adstock": geometric_adstock(initial_impact, beta, num_periods),
+                                    "Adstock": geometric_adstock_decay(initial_impact, beta, num_periods),
                                     ## Format adstock labels for neater plotting
-                                "Adstock Labels": [f'{x:,.0f}' for x in geometric_adstock(initial_impact, beta, num_periods)], 
+                                "Adstock Labels": [f'{x:,.0f}' for x in geometric_adstock_decay(initial_impact, beta, num_periods)], 
                                     ## Create column to label each adstock
                                     "Beta": f"Beta {i + 1}"})
 
@@ -164,9 +202,93 @@ with tab1:
                     title_font = dict(size = 30))
     st.plotly_chart(fig, theme="streamlit", use_container_width=False)
 
+# -------------------------- DELAYED GEOMETRIC ADSTOCK DISPLAY -------------------------
+with tab2:
+    st.header('Delayed Geometric Adstock Transformation')
+
+    st.markdown("Typical values for geometric adstock: \n \
+- TV: **:blue[0.3 - 0.8]** - decays slowly \n \
+- OOH/Print/Radio:  **:blue[0.1 - 0.4]** - decays moderately \n \
+- Digital:  **:blue[0.0 - 0.3]** - decays quickly \n")
+
+    # User inputs
+    st.subheader('User Inputs')
+    max_lag = st.slider(':blue[Number of weeks after impressions first received :alarm_clock:]', 1, 100, 30, key = "Delayed Geometric")
+    max_peak = st.slider(':blue[Number of weeks after impressions first received that max impact occurs :thermometer:]', 1, 100, 10, key = "delayed_geom_L")
+    # Let user choose decay rates to plot with
+    decay_rate_1 = st.slider(':blue[Beta 1] :large_blue_square:', 0.0, 1.0, 0.5, key="delay_decay")
+
+    # Add up to 2 more lines if the user wants it
+    st.markdown('**Would you like to show multiple (3) decay lines on the plot**')
+    multi_plot = st.checkbox('Okay! :grin:', key = "Delay Geom Multi")
+    # Create a list of decay rates
+    if multi_plot:
+        # Let user choose additional decay rates, lags and peaks to plot with
+        decay_rate_2 = st.slider(':red[Beta 2] :large_red_square:', 0.0, 1.0, 0.6, key="delay_decay2")
+        max_peak_2 = st.slider(':red[Number of weeks after impressions first received that max impact occurs :thermometer:]', 1, 100, 5, key = "delayed_geom_L 2")
+        max_lag_2 = st.slider(':red[Number of weeks after impressions first received :alarm_clock:]', 1, 100, 20, key = "Delayed Geometric 2 ")
+        decay_rate_3 = st.slider(':green[Beta 3] :large_green_square:', 0.0, 1.0, 0.9, key="delay_decay3")
+        max_lag_3 = st.slider(':green[Number of weeks after impressions first received :alarm_clock:]', 1, 100, 20, key = "Delayed Geometric 3 ")
+        max_peak_3 = st.slider(':green[Number of weeks after impressions first received that max impact occurs :thermometer:]', 1, 100, 5, key = "delayed_geom_L 3")
+
+        # Put in lists to iterate through later
+        decay_rates = [decay_rate_1, decay_rate_2, decay_rate_3]
+        lags = [max_lag, max_lag_2, max_lag_3]        
+        peaks = [max_peak, max_peak_2, max_peak_3]
+
+    else:
+        decay_rates = [decay_rate_1]
+        lags = [max_lag]
+        peaks = [max_peak]
+
+    # Create df to store each adstock in
+    all_adstocks = pd.DataFrame()
+    # Iterate through decay rates and generate df of values to plot
+    for i, beta in enumerate(decay_rates):
+        # Get geometric adstock values, decayed over time
+        adstock_df = pd.DataFrame({"Week": range(1, (lags[i] + 1)),
+                                ## Calculate adstock values
+                                    "Adstock": delayed_geometric_decay(impact = initial_impact,
+                                                                       decay_factor = beta,
+                                                                        theta = peaks[i],
+                                                                        L = lags[i]),
+                                    ## Format adstock labels for neater plotting
+                                "Adstock Labels": [f'{x:,.0f}' for x in delayed_geometric_decay(initial_impact, beta, peaks[i], lags[i])], 
+                                    ## Create column to label each adstock
+                                    "Beta": f"Beta {i + 1}"})
+
+        all_adstocks = pd.concat([all_adstocks, adstock_df])
+
+    # Plot adstock values
+    # Annotate the plot if user wants it
+    st.markdown('**Would you like to show the adstock values directly on the plot?**')
+    annotate = st.checkbox('Yes please! :pray:', key = "Delayed Geometric Annotate")
+    if annotate:
+        fig = px.line(all_adstocks, x = 'Week',
+                y = 'Adstock', text = 'Adstock Labels',
+                markers=True, color = "Beta",
+                # Replaces default color mapping by value
+                color_discrete_map={"Beta 1": "#636EFA",
+                                        "Beta 2": "#EF553B",
+                                        "Beta 3": "#00CC96"})
+        fig.update_traces(textposition="bottom left")
+    else:
+        fig = px.line(all_adstocks, x = 'Week',
+                y = 'Adstock',
+                markers=True, color = "Beta",
+                # Replaces default color mapping by value
+                color_discrete_map={"Beta 1": "#636EFA",
+                                        "Beta 2": "#EF553B",
+                                        "Beta 3": "#00CC96"})
+    # Format plot
+    fig.layout.height = 600
+    fig.layout.width = 1000
+    fig.update_layout(title_text="Geometric Adstock Decayed Over Weeks", 
+                    title_font = dict(size = 30))
+    st.plotly_chart(fig, theme="streamlit", use_container_width=False)
 
 # -------------------------- WEIBULL CDF ADSTOCK DISPLAY -------------------------
-with tab2:
+with tab3:
     st.header('Weibull CDF Adstock Transformation')
     # User inputs
     st.subheader('User Inputs')
@@ -246,7 +368,7 @@ with tab2:
     st.plotly_chart(fig, theme="streamlit", use_container_width=False)
 
 # -------------------------- WEIBULL PDF ADSTOCK DISPLAY -------------------------
-with tab3:
+with tab4:
     st.header('Weibull PDF Adstock Transformation')
 
     # User inputs
